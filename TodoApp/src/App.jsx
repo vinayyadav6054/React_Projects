9f8d2a77760d709d39344aff0dcc6809ce3c0ff9
vinayyadav6054 <yvinay7271@gmail.com>
2025-03-11 00:54:33 +0530

    Delete

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -10,7 +10,9 @@ const App = () => {
     console.log(tasks);
   }
   
-  
+  const deleteTask = () => {
+    setTasks
+  }
   return (
     <div className='w-full h-screen bg-violet-900 flex flex-col items-center justify-center'>
       <h1 className='text-4xl font-bold mb-4'>To Do App</h1>
