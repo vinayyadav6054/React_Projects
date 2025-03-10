70047e0c5619fea408dffa6b7e8b2719c59bf8be
Abhishek <abhishek.vish.code@gmail.com>
2025-03-11 00:37:54 +0530

    Completed the ui of todo list

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import TodoInput from './Components/TodoInput'
+import TodoList from './Components/TodoList'
 
 const App = () => {
   const [tasks, setTasks] =useState([])
@@ -7,13 +8,14 @@ const App = () => {
   const addTask = (newTask) => {
     setTasks([...tasks, newTask])
     console.log(tasks);
-    
-
   }
+  
+  
   return (
     <div className='w-full h-screen bg-violet-900 flex flex-col items-center justify-center'>
       <h1 className='text-4xl font-bold mb-4'>To Do App</h1>
       <TodoInput addTask={addTask} />
+      <TodoList tasks={tasks} />
     </div>
   )
 }
--- TodoApp/src/Components/TodoList.jsx
+++ TodoApp/src/Components/TodoList.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const TodoList = () => {
+const TodoList = ({tasks}) => {
   return (
-    <div>
+    <div className='w-[50%] h-[40%] bg-white rounded-md mt-3 p-3 '>
+      {tasks.length === 0 ? (
+        <p>No tasks added yet</p>
+      ) : (
+        tasks.map((task, index)=>(
+          <div className='flex justify-between items-center '>
+            <h1 key={index} className='text-zinc-900 border-b py-2'>{task}</h1>
+            <button className='text-xl'>❌</button>
+          </div>
+        ))
+      )}
       
     </div>
   )
